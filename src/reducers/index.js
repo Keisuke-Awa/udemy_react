c93224c0a9133a96894cc5d8c6e72a256ce1c314
keisukeawazu <keisuke.awa.basket@gmail.com>
2018-10-24 10:19:59 +0900

    finish

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,16 @@
 import { combineReducers } from 'redux';
+import queryString from 'query-string';
 
-const place = (state = '東京タワー', action) => {
+const getPlaceParam = () => {
+  const params = queryString.parse(location.search);
+  const { place } = params;
+  if (place && place.length > 0) {
+    return place;
+  }
+  return null;
+};
+
+const place = (state = getPlaceParam || '東京タワー', action) => {
   switch (action.type) {
     case 'CHANGE_PLACE':
       return action.place;
@@ -22,9 +32,38 @@ const geocodeResult = (
         address: action.address,
         location: action.location,
       };
+    case 'CHANGE_ERROR_MESSGE':
+      return {
+        address: action.message,
+        location: { lat: 0, lng: 0 },
+      };
     default:
       return state;
   }
 };
 
-export default combineReducers({ place, geocodeResult });
+const hotels = (state = [], action) => {
+  switch (action.type) {
+    case 'CHANGE_HOTELS':
+      return action.hotels;
+    default:
+      return state;
+  }
+};
+
+const sortKey = (state = 'price', action) => {
+  switch (action.type) {
+    case 'CHANGE_SORT_KEY':
+      return action.sortKey;
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({
+  place,
+  geocodeResult,
+  hotels,
+  sortKey,
+  getPlaceParam,
+});
